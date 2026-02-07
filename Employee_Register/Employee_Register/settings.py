2bfc14a2591243d39b95aa4f4e007ab263a3265f
parthigpm123 <parthigpm123@gmail.com.com>
2026-02-07 08:30:39 -0800

    moved

--- Employee_Register/Employee_Register/settings.py
+++ Employee_Register/Employee_Register/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fc8i9%pxl#(vbhm^qs!%7*i10u=a-=y(&v2#h98!j4#v$5baau'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
