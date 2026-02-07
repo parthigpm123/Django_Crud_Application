3d20d9999249535d138acd38d1684cac0e0ca876
parthigpm123 <parthigpm123@gmail.com.com>
2026-02-07 07:29:11 -0800

    django

--- Employee_Register/Employee_App/urls.py
+++ Employee_Register/Employee_App/urls.py
@@ -5,6 +5,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.employee_form),
-    path('list/', views.employee_list)
+    path('', views.employee_form,name='employee_insert'), # for inserting a new employee record
+    path('<int:id>/', views.employee_form,name='employee_update'),# for updating an existing employee record
+    path('delete/<int:id>/', views.employee_delete,name='employee_delete'),
+    path('list/', views.employee_list,name='employee_list')# for showing the list of employees
 ]
--- Employee_Register/Employee_Register/settings.py
+++ Employee_Register/Employee_Register/settings.py
@@ -38,7 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'Employee_App',
+    'crispy_forms',
+    'crispy_bootstrap4'
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
